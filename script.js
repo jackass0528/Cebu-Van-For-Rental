150b4c2a61dcaa06ea695e31e8f08c925979e70f
cebu taxi driver <cebufooddroid2015@gmail.com>
2023-03-06 19:51:04 +0800

    commit

--- script.js
+++ script.js
@@ -68,23 +68,6 @@
 
 
 
-const toggleButton = document.getElementsByClassName('toggle-button')[0]
-const navbarLinks = document.getElementsByClassName('navbar-link')[0]
-
-toggleButton.addEventListener('click', () => {
-    navbarLinks.classList.toggle("active")
-})
-
-
-const activePage = window.location.pathname;
-const navLinks = document.querySelectorAll('nav a').
-forEach(link => {
-if(link.href.include('${activePage}')){
-link.classList.add ('active');
-}
-})
-
-
 // facebook Page fb-customerchat 
 
 var chatbox = document.getElementById('fb-customer-chat');
