dda0bfc3f4f6957da7fa9d39af428ee8be2755f2
cebu taxi driver <cebufooddroid2015@gmail.com>
2023-04-03 19:12:46 +0800

    commit

--- loadmore.js
+++ loadmore.js
@@ -13,21 +13,3 @@ loadMoreBtn.style.display = 'none';
 
 }
 }
-
-// form js 
-function sendEmail(){
-   Email.send({
-      SecureToken: "08d08125-db06-40f5-83d6-1c182cf73011",
-      To : 'naldzaguirre@gamil.com',
-      From : document.getElementById("email").value,
-      Subject : "New Contact Form Enquiry",
-      Body : "Name: " + document.getElementById("name").value 
-      + "<br> Email" + document.getElementById("email").value
-      + "<br> Phone no:" + document.getElementById("phone").value
-      + "<br> Message" + document.getElementById("message").value
-
-  }).then(
-    message => alert("Message sent succesfully")
-  );
-
-}
