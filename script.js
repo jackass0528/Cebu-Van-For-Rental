ae617828806e8471b1c9ae9341c3438e4217d0f3
cebu taxi driver <cebufooddroid2015@gmail.com>
2023-09-05 16:26:26 +0800

    commit

--- script.js
+++ script.js
@@ -90,4 +90,27 @@ delete activeSlide.dataset.active
 })
 })
   
+// Back to top 
+
+const backToTopButton = document.querySelector("#back-to-top-btn");
+
+window.addEventListener("scroll", scrollFunction);
+
+function scrollFunction() {
+  if (window.pageYOffset > 300) { // show backToTopButton
+    backToTopButton.style.display = "block"
+  }
+  else { // Hide BackToTopButton
+    backToTopButton.style.display = "none"
+  }
+}
+  backToTopButton.addEventListener("click", backToTop);
+
+  function backToTop() {
+window.scrollTo(0, 0);
+  }
+
+
+
+
   
