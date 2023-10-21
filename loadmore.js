420c434c4e2bc4dbdc90c975e50f310e60242da6
cebu taxi driver <cebufooddroid2015@gmail.com>
2023-10-21 12:00:43 +0800

    commit

--- loadmore.js
+++ loadmore.js
@@ -13,3 +13,19 @@ loadMoreBtn.style.display = 'none';
 
 }
 }
+
+/*hide and show*/
+const button = 
+document.getElementById('toggleButton');
+const contentContainer = document.getElementById('contentContainer');
+
+button.addEventListener('click' , function() {
+   if (contentContainer.style.display === "none")
+   {
+      contentContainer.style.display = "block";
+   } else {
+      contentContainer.style.display = "none";
+   }
+});
+
+
