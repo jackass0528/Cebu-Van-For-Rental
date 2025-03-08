4c728e5b15e36b09c25a810432264dbd7a4f28a9
cebu taxi driver <cebufooddroid2015@gmail.com>
2025-03-08 09:02:01 +0800

    commiit

--- script.js
+++ script.js
@@ -128,24 +128,7 @@ fs.readdirSync(directory).forEach(file => {
   });
 
 
- /****
-  document.addEventListener('lazybeforesizes', function(e){
-    //calculate the size as a number
-    e.detail.width =  yourCalculation(e.target) || e.detail.width;
-  });
-  ****/
 
-  /****
-  var span = document.getElementsByTagName('span')[0];
-span.textContent = 'interactive'; // change DOM text content
-span.style.display = 'inline'; // change CSSOM property
-// create a new element, style it, and append it to the DOM
-var loadTime = document.createElement('div');
-loadTime.textContent = 'You loaded this page on: ' + new Date();
-loadTime.style.color = 'blue';
-document.body.appendChild(loadTime);
-
-****/
 
 
   
