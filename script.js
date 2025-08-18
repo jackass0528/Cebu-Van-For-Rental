bb46cba6bd8eebaaa0ada0c5b30438d282f5dfbc
Cebu Van for Rental <cebufooddroid2015@gmail.com>
2025-08-18 20:33:21 +0800

    commit

--- script.js
+++ script.js
@@ -116,17 +116,6 @@ window.scrollTo(0, 0);
 
 
 
-//Image sharp
-import sharp from 'sharp';
-import { readdirSync } from 'fs';
-const directory = './images';
-
-readdirSync(directory).forEach(file => {
-  sharp(`${directory}/${file}`)
-    .resize(200, 100) // width, height
-    .toFile(`${directory}/${file}-small.jpg`);
-  });
-
 
 
 
