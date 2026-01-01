70234da49574d46e97b5d8c035750a4070cb5ff6
Cebu Van for Rental <cebufooddroid2015@gmail.com>
2026-01-01 15:55:54 +0800

    commit

--- loadmore.js
+++ loadmore.js
@@ -1,13 +1,13 @@
 
 let loadMoreBtn = document.querySelector('#load-more');
-let currentItem = 2;
+let currentItem = 3;
 
 loadMoreBtn.onclick = () =>{
    let boxes =  [...document.querySelectorAll('.container .box-container .box')];
-   for (var i = currentItem; i < currentItem + 2; i++){
+   for (var i = currentItem; i < currentItem + 3; i++){
 boxes[i].style.display = 'inline-block';
    }
-   currentItem += 2;
+   currentItem += 3;
 if(currentItem >= boxes.length){
 loadMoreBtn.style.display = 'none';
 
